934894946e81f2fd816734b8ec5abcb88cee55da
Isaac Gardner <Isaacjamesgardner@hotmail.com>
2024-02-22 12:45:51 +0000

    first commit

---
+++ task_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    int n;
+	printf("Enter an integer: ");
+	scanf("%d", &n);
+	
+	
+	// Your code below here
+	printf("Vowel"); // place as needed in the code. You can use more than once.
+	print("Not vowel"); // place as needed in the code. You can use more than once.
+	
+	
+	// Do not edit below here
+	return 0;
+}
