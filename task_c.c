ff67b64030872e2d51879a165dbaee7811eddc4b
Isaac Gardner <Isaacjamesgardner@hotmail.com>
2024-02-22 13:19:22 +0000

    added error statements for invalid inputs

--- task_c.c
+++ task_c.c
@@ -6,12 +6,21 @@ int main(void){
 	
 	
 	// Your code below here
+	if (n < 0){ //cant have a negative index
+		printf("Error");
+		return 1;
+	}
+
+	if (n > 28){ //28 is the index of the last character in the array
+		printf("Error");
+		return 1;
+	}
+
 	char my_email[30] = "isaacjamesgardner@hotmail.com";
-	char letter = my_email[n]; //setting the character at index n to a new variable. there is no need to check if n is greater than 28 (the number of characters in my email)
-							   //as the code works fine without it, printing Not Vowel is such a case.	
-	char Vowels[5][1] = {"a", "e", "i", "o", "u"}; //I can check if the character is a vowel by searching for it within this array
+	char letter = my_email[n];
+	char Vowels[5][1] = {"a", "e", "i", "o", "u"}; //I can check if letter is a vowel by searching for it within this array
 	for (int i = 0; i < 5; i++){
-		if (letter == Vowels[i][0]){ //if it is in the Vowels[] array
+		if (letter == Vowels[i][0]){ //if it is in the Vowels[] array, print vowel and return
 			printf("Vowel");
 			return 0; //exiting the code early. if its found in the array, i want the code to exit so that it doesn't print "Not vowel" after the for loop is done.
 		}
